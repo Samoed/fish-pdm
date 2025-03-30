d365f00de81f2a654a45e575021e44751b103f25
Roman Solomatin <samoed.roman@gmail.com>
2025-03-30 03:31:59 +0300

    correct venv in subfolder

--- conf.d/fish-python-autovenv.fish
+++ conf.d/fish-python-autovenv.fish
@@ -15,7 +15,7 @@ end
 function __has_venv_directory
     # List of directories to check for virtual environments
     set -l venv_dirs "venv" ".venv" ".virtualenv" "env"
-    
+
     for dir in $venv_dirs
         if test -e "$PWD/$dir"
             echo $dir
@@ -25,57 +25,68 @@ function __has_venv_directory
     return 1
 end
 
+# Function to check if current directory is within a project root directory
+function __is_subdir_of
+    set -l current_path $PWD
+    set -l project_root $argv[1]
+
+    # Use string match to check if current path starts with project root
+    string match -q "$project_root*" "$current_path"
+    return $status
+end
+
 function __python_venv_activate --on-variable PWD
     if status --is-command-substitution
         return
     end
 
-    # Get current venv directory if any
+    # Check if we are in a directory with a venv
     set -l venv_dir (__has_venv_directory)
     set -l has_venv $status
 
-    # Deactivation check: if moving out of a directory with active venv, deactivate it
-    if test $has_venv -ne 0
-        if set -q __python_venv_initial_pwd
-            # Check if 'deactivate' command exists and call it if so
+    # If we have a venv in the current directory and no environment is active yet
+    if test $has_venv -eq 0 && not set -q __python_venv_initial_pwd
+        echo "Activating Python virtual environment..."
+        set -gx __python_venv_initial_pwd "$PWD"
+        set -gx __python_venv_root_dir "$PWD"
+
+        # Check for activate.fish in the venv directory
+        if test -e "$PWD/$venv_dir/bin/activate.fish"
+            source "$PWD/$venv_dir/bin/activate.fish"
+        else if test -e "$PWD/$venv_dir/Scripts/activate.fish"  # For Windows
+            source "$PWD/$venv_dir/Scripts/activate.fish"
+        else
+            echo "Could not find activation script for Python virtual environment."
+            set -e __python_venv_initial_pwd
+            set -e __python_venv_root_dir
+            return 1
+        end
+
+        # Load environment variables if .env file exists and FISH_VENV_LOAD_ENV is set
+        if test "$FISH_VENV_LOAD_ENV" -a -e "$PWD/.env"
+            echo "Setting environment variables from .env file..."
+            posix-source $PWD/.env
+        end
+        return
+    end
+
+    # Deactivation check: only if we have moved outside the project root directory
+    if set -q __python_venv_root_dir
+        if not __is_subdir_of "$__python_venv_root_dir"
+            # We've moved outside of the project directory, deactivate the environment
             if type -q deactivate
                 deactivate
                 echo "Python virtual environment deactivated."
             end
 
             set -e __python_venv_initial_pwd
+            set -e __python_venv_root_dir
 
             # Clear the list of loaded variable names
             for var_name in $__venv_loaded_vars
                 set -e $var_name
             end
             set -e __venv_loaded_vars
-        end
-        return
-    end
-
-    # Activation
-    if test $has_venv -eq 0
-        if not set -q __python_venv_initial_pwd
-            echo "Activating Python virtual environment..."
-            set -gx __python_venv_initial_pwd "$PWD"
-            
-            # Check for activate.fish in the venv directory
-            if test -e "$PWD/$venv_dir/bin/activate.fish"
-                source "$PWD/$venv_dir/bin/activate.fish"
-            else if test -e "$PWD/$venv_dir/Scripts/activate.fish"  # For Windows
-                source "$PWD/$venv_dir/Scripts/activate.fish"
-            else
-                echo "Could not find activation script for Python virtual environment."
-                set -e __python_venv_initial_pwd
-                return 1
-            end
-
-            # Load environment variables if .env file exists and FISH_VENV_LOAD_ENV is set
-            if test "$FISH_VENV_LOAD_ENV" -a -e "$PWD/.env"
-                echo "Setting environment variables from .env file..."
-                posix-source $PWD/.env
-            end
         end
     end
 end
